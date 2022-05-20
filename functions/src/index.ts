ae210012aeb7f79a9cac47dfd453360cd50dff23
Aaren Chu <aarenchu@gmail.com>
2022-05-20 15:20:55 -0400

    Changed to express and implemented add player

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,12 +5,25 @@ import * as firestore from 'firebase-admin/firestore';
 const app = admin.initializeApp();
 const fs = firestore.getFirestore(app);
 const playerCollectionRef = fs.collection('players');
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.getPlayers = functions.https.onRequest(async (req, res) => {
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+
+const expressApp = express();
+
+// Automatically allow cross-origin requests
+expressApp.use(cors({ origin: true }));
+
+// Add middleware to authenticate requests
+// expressApp.use(myMiddleware);
+
+expressApp.use(bodyParser.json());
+
+// getPlayers
+expressApp.get('/', async (req, res) => {
   await playerCollectionRef.get().then((querySnapshot) => {
     if (querySnapshot.empty) res.json('No documents found');
     else {
@@ -23,3 +36,30 @@ exports.getPlayers = functions.https.onRequest(async (req, res) => {
     }
   });
 });
+
+// addPlayer
+expressApp.post('/', async (req, res) => {
+  // Grab the text parameter.
+  console.log('this is the req body');
+  console.log(req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+  let newPlayer = {
+    username: username,
+    password: password,
+    // TODO: Profile pic
+  };
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await playerCollectionRef.add(newPlayer);
+  // Send back a message that we've successfully written the message
+  res.json({ result: `Player with ID: ${writeResult.id} added.` });
+});
+
+// update player colour
+// expressApp.put('/:id', (req, res) =>
+//   res.send(Widgets.update(req.params.id, req.body))
+// );
+
+// expressApp.get('/:id', (req, res) => res.send(Widgets.list()));
+
+exports.players = functions.https.onRequest(expressApp);
